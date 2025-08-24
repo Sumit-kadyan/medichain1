ea8e7397462aaf6b9ad76a83a19c31f69566f44f
sumit kadyan <sumitkadyan1323@gmail.com>
2025-08-24 07:34:34 +0000

    hey i have created a new database named medichain-of3u, just fetch this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,11 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "medichain-of3ue",
+  projectId: "medichain-of3u",
   appId: "1:250475667538:web:2324574f92d6d5a34bff8b",
-  storageBucket: "medichain-of3ue.firebasestorage.app",
+  storageBucket: "medichain-of3u.appspot.com",
   apiKey: "AIzaSyAyYsik0fydSxsBvnSs5StPQ4fCjKB_TMc",
-  authDomain: "medichain-of3ue.firebaseapp.com",
+  authDomain: "medichain-of3u.firebaseapp.com",
   messagingSenderId: "250475667538"
 };
 
