4ae4eba6f9dcdec74ae2af32599a4863cd6349f8
sumit kadyan <sumitkadyan1323@gmail.com>
2025-12-10 04:02:15 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@ import { FirebaseOptions } from 'firebase/app';
 
 // This configuration is replaced by the bootstrapper with your project's config.
 export const firebaseConfig: FirebaseOptions = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: 'medichain-of3ue',
+  appId: '1:250475667538:web:2324574f92d6d5a34bff8b',
+  apiKey: 'AIzaSyDtYoi1x923bcZ8eNnjw79lPY_dQxf9gGk',
+  authDomain: 'medichain-of3ue.firebaseapp.com',
+  storageBucket: 'medichain-of3ue.appspot.com',
+  messagingSenderId: '250475667538',
 };
