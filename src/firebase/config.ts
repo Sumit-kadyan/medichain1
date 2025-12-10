fba98466fc6423cea47a7e3bb730e68a290d209c
sumit kadyan <sumitkadyan1323@gmail.com>
2025-12-10 08:11:02 +0000

    now i have made a .env file keep the security keys there and call with `

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,14 +1,14 @@
+
 // src/firebase/config.ts
 import { FirebaseOptions } from 'firebase/app';
 
-// WARNING: This is a temporary solution to get the application running.
-// It is not secure to keep API keys in source code.
-// We will implement a secure solution using environment variables next.
+// This configuration now securely loads from environment variables.
+// Make sure you have a .env file in your project root with the required keys.
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDtYoi1x923bcZ8eNnjw79lPY_dQxf9gGk",
-  authDomain: "medichain-of3ue.firebaseapp.com",
-  projectId: "medichain-of3ue",
-  storageBucket: "medichain-of3ue.appspot.com",
-  messagingSenderId: "250475667538",
-  appId: "1:250475667538:web:2324574f92d6d5a34bff8b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
