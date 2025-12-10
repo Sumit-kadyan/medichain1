8997b7eaa6f0dc2927cdf4ab5e28ffca8592ca34
sumit kadyan <sumitkadyan1323@gmail.com>
2025-12-10 08:07:37 +0000

    hey we don't have any .env file !!!

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,14 @@
 // src/firebase/config.ts
 import { FirebaseOptions } from 'firebase/app';
 
-// This configuration is now populated from environment variables
-// to keep secret keys out of the source code.
+// WARNING: This is a temporary solution to get the application running.
+// It is not secure to keep API keys in source code.
+// We will implement a secure solution using environment variables next.
 export const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyDtYoi1x923bcZ8eNnjw79lPY_dQxf9gGk",
   authDomain: "medichain-of3ue.firebaseapp.com",
   projectId: "medichain-of3ue",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: "medichain-of3ue.appspot.com",
   messagingSenderId: "250475667538",
   appId: "1:250475667538:web:2324574f92d6d5a34bff8b",
 };
