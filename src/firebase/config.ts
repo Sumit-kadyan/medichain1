145841822ba9da786d7c3c5a0c05a5656859e49a
sumit kadyan <sumitkadyan1323@gmail.com>
2025-12-10 04:47:38 +0000

    hey all the security keys are there in src/firebase/config.ts and src/l

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,13 @@
 // src/firebase/config.ts
 import { FirebaseOptions } from 'firebase/app';
 
-// This configuration is replaced by the bootstrapper with your project's config.
+// This configuration is now populated from environment variables
+// to keep secret keys out of the source code.
 export const firebaseConfig: FirebaseOptions = {
-  projectId: 'medichain-of3ue',
-  appId: '1:250475667538:web:2324574f92d6d5a34bff8b',
-  apiKey: 'AIzaSyDtYoi1x923bcZ8eNnjw79lPY_dQxf9gGk',
-  authDomain: 'medichain-of3ue.firebaseapp.com',
-  storageBucket: 'medichain-of3ue.appspot.com',
-  messagingSenderId: '250475667538',
+  apiKey: "AIzaSyDtYoi1x923bcZ8eNnjw79lPY_dQxf9gGk",
+  authDomain: "medichain-of3ue.firebaseapp.com",
+  projectId: "medichain-of3ue",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: "250475667538",
+  appId: "1:250475667538:web:2324574f92d6d5a34bff8b",
 };
