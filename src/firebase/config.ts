183203378ec212658543ccc31574747080ba0e16
sumit kadyan <sumitkadyan1323@gmail.com>
2025-12-10 00:14:05 +0000

    hey i have done the push but our api key is public now !!

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@ import { FirebaseOptions } from 'firebase/app';
 
 // This configuration is replaced by the bootstrapper with your project's config.
 export const firebaseConfig: FirebaseOptions = {
-  projectId: 'medichain-of3ue',
-  appId: '1:250475667538:web:2324574f92d6d5a34bff8b',
-  storageBucket: 'medichain-of3ue.firebasestorage.app',
-  apiKey: 'AIzaSyAyYsik0fydSxsBvnSs5StPQ4fCjKB_TMc',
-  authDomain: 'medichain-of3ue.firebaseapp.com',
-  messagingSenderId: '250475667538',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
