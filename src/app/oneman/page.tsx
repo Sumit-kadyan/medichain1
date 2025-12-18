2e26e99284b33e508ccdd4174802025ceeefcacc
sumit kadyan <sumitkadyan1323@gmail.com>
2025-12-18 08:12:05 +0000

    modified:   src/app/doctor/page.tsx 	modified:   src/app/login/page.tsx 	modified:   src/app/oneman/page.tsx 	modified:   src/app/page.tsx 	modified:   src/app/pharmacy/page.tsx 	modified:   src/app/reception/page.tsx

--- src/app/oneman/page.tsx
+++ src/app/oneman/page.tsx
@@ -1,6 +1,6 @@
-
-
 'use client';
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect, useRef } from 'react';
 import { useClinicContext, Patient, Doctor, BillDetails, Prescription } from '@/context/clinic-context';
 import { useToast } from '@/hooks/use-toast';
